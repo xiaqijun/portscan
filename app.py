107f0385baf6da6e983d07aae16727968de8f0f9
xiaqiqi <xiaqiqi@outlook.com>
2025-04-06 21:28:28 +0800

    新增返回结果的接口

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,send_file
 from flask_migrate import Migrate
 from flask_apscheduler import APScheduler
 from scan import Portscan
 from datetime import datetime,timedelta
 import uuid
 from model import Task, db
+import json
 scanners = {}
 migrate=Migrate()
 scheduler=APScheduler()
@@ -36,16 +37,16 @@ def add_task():
         )
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
+    result_file=scanner.get_results()
     task = Task(
         name=task_name,
         task_id=id,
         status='running',
-        result_file=None,
+        result_file=result_file,
         created_at=datetime.now()
     )
     db.session.add(task)
     db.session.commit()
-
     return {'status': 'success', 'message': '任务添加成功', 'task_id': id}
 
 def add_task( id,scanner):
@@ -62,13 +63,24 @@ def add_task( id,scanner):
 def stop_task():
     task_id = request.json.get('task_id')
     scanner=scanners.get(task_id)
-
     if scanner:
         scanner.stop()
         return {'status': 'success', 'message': '任务停止成功'}
     else:
         return {'status': 'error', 'message': '任务不存在或已完成'}
 
+@app.route('/get_task_result', methods=['GET'])
+def get_task_result():
+    task_id = request.args.get('task_id')
+    task = Task.query.filter_by(task_id=task_id).first()
+    result=[]
+    if task:
+        try:
+            return send_file(task.result_file, as_attachment=True)
+        except Exception as e:
+            return {'status': 'error', 'message': f'无法提供下载: {str(e)}'}
+    else:
+        return {'status': 'error', 'message': '任务不存在或已完成'}
 if __name__ == '__main__':
     host = '127.0.0.1'
     app.run(host=host, port=5000, debug=True)
